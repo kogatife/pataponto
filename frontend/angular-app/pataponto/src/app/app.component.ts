0fb06b1bb6782d696e4edfd42c46556186e89950
kogatife <fekogati@gmail.com>
2023-11-30 01:24:50 -0300

    adding new features

--- frontend/angular-app/pataponto/src/app/app.component.ts
+++ frontend/angular-app/pataponto/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', './carousel.css', '../../node_modules/bootstrap/dist/css/bootstrap.min.css']
 })
 export class AppComponent {
   title = 'pataponto';
